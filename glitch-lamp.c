220190117ba9fe68170f783fee37a9de1741398c
Tim Gremalm <tim@gremalm.se>
2018-07-17 17:15:36 +0200

    Glitch statemachine that randomizes glitches

--- glitch-lamp.c
+++ glitch-lamp.c
@@ -22,6 +22,19 @@
 e131_packet_t pbuff; /* Packet buffer */
 e131_packet_t *pwbuff; /* Pointer to working packet buffer */
 
+//Set up PWM for pins
+uint8_t pins[] = {2, 4}; //NodeMCU D4, D2 https://github.com/nodemcu/nodemcu-devkit-v1.0#pin-map
+uint8_t pinDutyOut[] = {0, 0};
+
+//Glitch state machine parameters
+uint32_t glitchNextGlitch[] = {(uint32_t)0, (uint32_t)0};
+uint8_t glitchGlitchEnabled[] = {1, 0};
+uint16_t glitchGlitchRandomMin[] = {(uint16_t)10000, (uint16_t)222000};
+uint16_t glitchGlitchRandomMax[] = {(uint16_t)11000, (uint16_t)400000};
+
+//DMX Channel parameter
+uint8_t dmxChannelStart = 1;
+
 void e131task(void *pvParameters) {
 	printf("Open server.\r\n");
 	vTaskDelay(1000);
@@ -76,10 +89,8 @@ void e131task(void *pvParameters) {
 	}
 }
 
-void pwmtask(void *pvParameters)
-{
+void pwmtask(void *pvParameters) {
 	//Set up PWM for pins
-	uint8_t    pins[] = {2}; //NodeMCU D4 https://github.com/nodemcu/nodemcu-devkit-v1.0#pin-map
 	pwm_info_t pwm_info;
 	pwm_info.channels = sizeof(pins);
 	pwm_info.reverse = false;
@@ -93,20 +104,79 @@ void pwmtask(void *pvParameters)
 
 		uint16_t channel[pwm_info.channels];
 		for (uint8_t i=0; i<pwm_info.channels; i++) {
-			channel[i] = pwbuff->property_values[i+5]; //Get DMX channel value from sACN struct
+			channel[i] = pinDutyOut[i]; //Current duty cycle to be set on channel
 			//Upscale 8 bit DMX value to 16 bit, and add original value to fit the range from 0-65535
 			channel[i] = (channel[i] << 8) + channel[i];
 			multipwm_set_duty(&pwm_info, i, channel[i]);
 		}
 
-		//Only print values once every modula
-		if ((rand() % 50) == 0) {
-			for (uint8_t i=0; i<pwm_info.channels; i++) {
-				printf("Channel %d: %d\n", i+1, channel[i]);
+		multipwm_start(&pwm_info);
+		vTaskDelay(1);
+	}
+}
+
+uint16_t randomRange(uint16_t minimum_number, uint16_t max_number) {
+	return rand() % (max_number + 1 - minimum_number) + minimum_number;
+}
+
+void calculateNextGlitch(uint8_t id) {
+	uint32_t time = sys_now();
+	bool makeShortGlitch = false;
+	//If level is off, it should generate a short glitch to light up soon
+	if (pinDutyOut[id] == 0) {
+		makeShortGlitch = true;
+	} else {
+		//If on, randimize if to make a short or a long glitch
+		if(randomRange(0, 2) == 0) {
+			makeShortGlitch = false;
+		} else {
+			makeShortGlitch = true;
+		}
+	}
+	uint16_t randomDelay = 0;
+	if (makeShortGlitch) {
+		randomDelay = randomRange(60, 300);
+	} else {
+		randomDelay = randomRange(glitchGlitchRandomMin[id], glitchGlitchRandomMax[id]);
+	}
+	glitchNextGlitch[id] = time + randomDelay;
+	printf("Next delay in: %dms\n", randomDelay);
+}
+
+void glitch(uint8_t id) {
+	//Toggle level for a glitch effect
+	if(pinDutyOut[id] == 0) {
+		pinDutyOut[id] = pwbuff->property_values[id + dmxChannelStart]; //Get DMX channel value from sACN struct
+	} else {
+		pinDutyOut[id] = 0;
+	}
+}
+
+void checkLevelAgainstDMX(uint8_t id) {
+	//Only update level if it's not in an off-state
+	if(pinDutyOut[id] != 0) {
+		pinDutyOut[id] = pwbuff->property_values[id + dmxChannelStart]; //Get DMX channel value from sACN struct
+	}
+}
+
+void glitchtask(void *pvParameters) {
+	//Set up glitching
+	for (uint8_t i = 0; i < sizeof(pins); i++) {
+		pinDutyOut[i] = 0;
+	}
+
+	while(1) {
+		uint32_t time = sys_now();
+		for (uint8_t i = 0; i < sizeof(pins); i++) {
+			if (glitchGlitchEnabled[i] == 1) {
+				if (time >= glitchNextGlitch[i]) {
+					printf("Glitch %d\n", i);
+					glitch(i);
+					calculateNextGlitch(i);
+				}
+				checkLevelAgainstDMX(i);
 			}
 		}
-
-		multipwm_start(&pwm_info);
 		vTaskDelay(1);
 	}
 }
@@ -126,5 +196,6 @@ void user_init(void) {
 	pwbuff = &pbuff;
 	xTaskCreate(e131task, "e131task", 768, NULL, 8, NULL);
 	xTaskCreate(pwmtask, "pwmtask", 256, NULL, 2, NULL);
+	xTaskCreate(glitchtask, "glitchtask", 256, NULL, 2, NULL);
 }
 
